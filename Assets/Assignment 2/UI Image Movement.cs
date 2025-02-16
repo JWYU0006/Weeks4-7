65c7dd8b92c1a9920129f13f3d04bb12fd25bbd5
YJW\34733 <yujiawe@sheridancollege.ca>
2025-02-15 20:42:00 -0500

    create UI canvas.
    sky move left over time.

---
+++ Assets/Assignment 2/UI Image Movement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class UIImageMovement : MonoBehaviour
+{
+    RectTransform m_RectTransform;      //UI Image game object
+    public float speed;                 //Sky speed, also time speed
+    float distance;                     //how far sky has moved
+    public int imageWidth;              //pixels of image width
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_RectTransform = GetComponent<RectTransform>();        //fetch RectTransform from UI Image game object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance -= Time.deltaTime * speed;         //sky move left over time.
+        m_RectTransform.anchoredPosition = new Vector2(distance, 0);        //set horizontal anchor position
+    }
+}
